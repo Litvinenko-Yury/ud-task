39fed20bf5f7afa183dfe20e708e7b6c54a25c76
Литвиненко Юрий <litvinenko-don@ukr.net>
2020-02-12 16:22:46 +0300

    change script.js

--- js/script.js
+++ js/script.js
@@ -20,17 +20,11 @@ let appData = {
 
 
 /*===4===*/
-let question1_a = prompt("Введите обязательную статью расходов в этом месяце", "еда");
-let question1_b = Number(prompt("Во сколько обойдется?", "600"));
-console.log("question1_a: " + question1_a);
-console.log("question1_b: " + question1_b);
+let question1_a = prompt("Введите обязательную статью расходов в этом месяце", "еда"),
+    question1_b = Number(prompt("Во сколько обойдется?", "600")),
+    question2_a = prompt("Введите обязательную статью расходов в этом месяце", "связь"),
+    question2_b = Number(prompt("Во сколько обойдется?", "60"));
 console.log(typeof (question1_b));
-
-
-let question2_a = prompt("Введите обязательную статью расходов в этом месяце", "связь");
-let question2_b = Number(prompt("Во сколько обойдется?", "60"));
-console.log("question2_a: " + question2_a);
-console.log("question2_b: " + question2_b);
 console.log(typeof (question2_b));
 
 appData.expenses[question1_a] = question1_b;
@@ -38,5 +32,3 @@ appData.expenses[question2_a] = question2_b;
 
 let totalCost = (question1_b + question2_b) / 30;
 console.log("бюджет на 1 день: " + totalCost);
-
-alert("бюджет на 1 день = " + totalCost);
